6c7f2856a03bcb0b91fc1ff01a1f0408839dfe7f
LivingInABubble <54778084+LivingInABubble@users.noreply.github.com>
2022-05-05 17:27:22 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,30 +1,21 @@
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Form
+from fastapi.responses import JSONResponse
 
 from model import ChatBot
 
 app = FastAPI()
 
-# mounts the static folder that contains the css file
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# locates the template files that will be modified at run time
-# with the dialog form the user and bot
-templates = Jinja2Templates(directory="templates")
-
-
-@app.post("/", response_class=HTMLResponse)
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request, message: Optional[str] = Form(None)):
+@app.post("/", response_class=JSONResponse)
+@app.get("/", response_class=JSONResponse)
+async def root(message: Optional[str] = Form(None)):
     # if the Form is not None, then get a reply from the bot
     if message is not None:
         # gets a response of the AI bot
-        return chatbot.get_reply(message)
+        return {'reply': chatbot.get_reply(message)}
 
 
 # initialises the chatbot model and starts the uvicorn app
